d71669416b42e30a5cbf968244bf300af37f5ef2
Adams <1247147302@mil>
2016-05-20 11:46:05 -0400

    second commit

---
+++ Postalux_Wpf/General.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postalux_Wpf
+{
+    class General
+    {
+        public string NormalizePath(string Path_Orig)
+        {
+            if (Path_Orig.EndsWith("\\"))
+            {
+                return Path_Orig;
+            }
+            else
+            {
+                return Path_Orig + "\\";
+            }
+        }
+
+        /// <summary>
+        /// Determine whether a string represents a valid decimal value.
+        /// </summary>
+        /// <param name="Text"></param>
+        /// <returns></returns>
+        public static bool IsTextNumeric(string Text)
+        {
+            // ToDo: Make sure there is only one decimal point and only one minus sign.
+
+            bool b_OK = true;
+
+            if (Text.Length > 0)
+            {
+                // Test the first character:
+                string c = Text[0].ToString();
+                if (!"-0123456789.".Contains(c)) b_OK = false;
+
+                for (int Pos = 1; Pos < Text.Length; Pos++)
+                {
+                    c = Text[Pos].ToString();
+                    if (!"0123456789.".Contains(c)) b_OK = false;
+                }
+
+                if (b_OK)
+                {
+                    // Count the decimal points in the text.
+                    int count = Text.Split('.').Length - 1;
+                    if (count > 1) b_OK = false;
+                }
+            }
+            return b_OK;
+        }
+
+        /// <summary>
+        /// Format an amount of money using the dollar sign if it's at least a dollar, or the cents sign if it's less than a dollar.
+        /// </summary>
+        /// <param name="dec_Val">amount of money in dollars</param>
+        /// <returns>formatted string</returns>
+        public string FormatDollarsOrCents(decimal dec_Val)
+        {
+            string str_Val;
+
+            if (dec_Val < 1)
+            {
+                str_Val = (dec_Val * 100).ToString("F0") + "¢";
+            }
+            else
+            {
+                str_Val = dec_Val.ToString("C2");
+            }
+
+            return str_Val;
+        }
+    }
+}
---
+++ Postalux_Wpf/Postalux.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Postalux_Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Postalux : Window
+    {
+        General _general = new General();
+
+        public Postalux()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Display string representations of numbers for en-us culture.
+                CultureInfo ci = new CultureInfo("en-us");
+
+                string Path_App = System.AppDomain.CurrentDomain.BaseDirectory;
+
+                XmlDocument xmldoc = new XmlDocument();
+                FileStream fs = new FileStream(Path_App + "..\\..\\App_Data\\Cards.xml", FileMode.Open, FileAccess.Read);
+                xmldoc.Load(fs);
+
+                XmlNodeList xnList = xmldoc.SelectNodes("/Cards/Card");
+                List<CardDisplay> items = new List<CardDisplay>();
+
+                foreach (XmlNode xn in xnList)
+                {
+                    // Gather data about this card:
+                    Int32 i_ScottUxNum_List = Convert.ToInt32(xn["ScottUxNum"].InnerText);
+                    decimal dec_FaceVal = Convert.ToDecimal(xn["FaceVal"].InnerText);
+                    string str_FaceVal = _general.FormatDollarsOrCents(dec_FaceVal);
+                    string str_Descr = xn["Descr"].InnerText;
+                    string str_Issued = "";
+                    if (xn["Issued"] != null) str_Issued = xn["Issued"].InnerText;
+                    string FileName_Card = i_ScottUxNum_List.ToString("D3", ci) + ".jpeg";
+                    string RelPathedFileName_Card = "..\\Images\\Data\\" + FileName_Card;
+                    string PathedFileName_Card = "C:\\Users\\Michael.C.Adams\\PD\\Postalux\\Postalux\\Images\\Data\\" + FileName_Card;
+
+                    // Add the data to the listview item:
+                    items.Add(new CardDisplay() { ScottNum = "UX" + i_ScottUxNum_List.ToString(), FaceVal = str_FaceVal, Descr = str_Descr, Issued = str_Issued });
+                }
+                lv_Cards.ItemsSource = items;
+
+                CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lv_Cards.ItemsSource);
+                view.Filter = UserFilter;
+
+                btn_Series_All.IsChecked = true;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private bool UserFilter(object item)
+        {
+            if (String.IsNullOrEmpty(textbox_SearchByDescr.Text)) return true;
+            else return ((item as CardDisplay).Descr.IndexOf(textbox_SearchByDescr.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+        }
+
+        private void textbox_SearchByDescr_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(lv_Cards.ItemsSource).Refresh();
+        }
+    }
+
+    public class CardDisplay
+    {
+        public string ScottNum { get; set; }
+
+        public string FaceVal { get; set; }
+
+        public string Descr { get; set; }
+
+        public string Issued { get; set; }
+    }
+}
